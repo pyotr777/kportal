85f4b89d93c4e84b12748684181b9a5c72a449ed
Peter Bryzgalov <peterbryz@yahoo.com>
2016-05-06 10:40:50 +0900

    Start docker script: install bridge only if not exists

--- src/release/start_server.sh
+++ src/release/start_server.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
 # Start apache
-~/usr/local/apache/bin/apachectl start
+sudo service httpd start
 
 # Stop docker
-sudo service docker stop;
+sudo service docker stop
 
 # Start bridge & start docker 
-sudo brctl addbr bridge0
-sudo ip addr add 10.0.0.1/8 dev bridge0
-sudo ip link set dev bridge0 up
-ip addr show bridge0
-sudo docker -d -H 127.0.0.1:9555 -b=bridge0 &
-
+if [ ! "$(brctl show | grep bridge0)" ]; then
+    sudo brctl addbr bridge0
+    sudo ip addr add 10.0.0.1/8 dev bridge0
+    sudo ip link set dev bridge0 up
+    ip addr show bridge0
+fi
+docker -H 127.0.0.1:9555 images &>/dev/null
+if [[ "$?"=="1" ]]; then
+    sudo docker -d -H 127.0.0.1:9555 -b=bridge0 &
+fi
