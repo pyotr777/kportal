63552e4189b6cc0a9d520e81c37020e6abc8d44e
Peter Bryzgalov <peterbryz@yahoo.com>
2016-05-31 18:51:24 +0900

    Reverted files customised for use with AWS Kportal instance and TravisCI.

--- src/release/kp_server.sh
+++ src/release/kp_server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ -z  "$LD_LIBRARY_PATH" ]; then 
-	LD_LIBRARY_PATH=/home/anlab/usr/local/lib
+	LD_LIBRARY_PATH=/home/kportal//usr/local/lib
 else
-	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/anlab/usr/local/lib
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kportal//usr/local/lib
 fi
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kportal/usr/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kportal/usr/lib
 
 export LD_LIBRARY_PATH;
 #export $LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
-/bin/kp_server $@ 1>/etc/kportal/log/stdout.log 2>/etc/kportal/log/stderr.log
+/bin/kp_server $@ 1>/home/kportal/log/stdout.log 2>/home/kportal/log/stderr.log
