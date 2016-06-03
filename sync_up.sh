4f5f5bb4c3d9a9df3b03ae88e814453cc8344258
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-03 10:53:26 +0900

    Updated synchronisation scripts: introduced command-line parameter.

--- sync_up.sh
+++ sync_up.sh
@@ -4,5 +4,4 @@ if [[ $1 == "do" ]]; then
 else
     echo "Dry run with options \"$OPT\". To actually transfer files execute with \"do\" option: > $0 do"
 fi
-
-rsync $OPT --exclude-from 'rsyncexclude.txt' kportal:/home/kportal/Release_20160531/src/ src/
+rsync $OPT --exclude-from 'rsyncexclude.txt' src/ kportal:/home/kportal/Release_20160531/src/
