5c64f272aebc0e356d4b26393890eee0465c08bd
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-06 17:15:51 +0900

    Matrix multiplication sample App with plain and tau-instrumented binaries.

---
+++ sample_application/matmult/job_template.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#PJM --rsc-list "node=[NUMBER_OF_NODE]"
+#PJM --rsc-list "elapse=[MAX_ELAPSE_TIME]"
+#PJM --stg-transfiles "all"
+#PJM --mpi "use-rankdir"
+#PJM --stgin "rank=* ./[EXE_FILE] %r:./"
+#PJM --stgin-dir "rank=* ./[STAGE_IN_DIR] %r:./"
+#PJM --stgout-dir "rank=* ./ ./[STAGE_OUT_DIR]"
+#PJM -s
+. /work/system/Env_base
+mpiexec ./[EXE_FILE] [EXE_ARGS]
+
