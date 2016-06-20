3483c968bc316424917585468302a61691615690
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-21 08:28:09 +0900

    Start kp_server with user kportal home directory detection. Create log directory if not exists.

--- src/release/kp_server.sh
+++ src/release/kp_server.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+HOME_DIR=$(sudo -u kportal echo $HOME)
+LOGDIR="$HOME_DIR/log"
+echo "Starting kp_server. HOME_DIR=$HOME_DIR, LOGDIR=$LOGDIR."
 if [ -z  "$LD_LIBRARY_PATH" ]; then 
-	LD_LIBRARY_PATH=/home/kportal//usr/local/lib
+	LD_LIBRARY_PATH=$HOME_DIR/usr/local/lib
 else
-	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kportal//usr/local/lib
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME_DIR/usr/local/lib
 fi
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kportal/usr/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME_DIR/usr/lib
 
 export LD_LIBRARY_PATH;
-#export $LD_LIBRARY_PATH
+
 echo $LD_LIBRARY_PATH
-/bin/kp_server $@ 1>/home/kportal/log/stdout.log 2>/home/kportal/log/stderr.log
+if [ ! -d "$LOGDIR" ]; then
+	mkdir -p "$LOGDIR"
+fi
+/bin/kp_server $@ 1>$LOGDIR/stdout.log 2>$LOGDIR/stderr.log
