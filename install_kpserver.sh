9dffee08e0286595cb5edc3136da1f7584cc6774
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-22 13:19:38 +0900

    Use KP_HOME directory in some commands where current dir was not explicitly indicated.

--- install_kpserver.sh
+++ install_kpserver.sh
@@ -3,16 +3,16 @@
 
 ORG_DIR=$(pwd)
 echo "Came from $ORG_DIR"
-HOME_DIR=$(sudo su kportal -c 'echo $HOME')
-echo "HOME_DIR=$HOME_DIR"
-INSTALL_DIR="$HOME_DIR/install"
+KP_HOME=$(sudo su kportal -c 'echo $HOME')
+echo "KP_HOME=$KP_HOME"
+INSTALL_DIR="$KP_HOME/install"
 sudo mkdir -p "$INSTALL_DIR"
 sudo chown -R kportal:kportal "$INSTALL_DIR"
 cd "$INSTALL_DIR"
 echo "Saving boost in $(pwd)."
 export BOOSTVERSION="1.60.0"
 export BOOSTARCHIVE="boost_1_60_0"
-echo "Installing BOOST $BOOSTVERSION into $HOME_DIR/usr"
+echo "Installing BOOST $BOOSTVERSION into $KP_HOME/usr"
 if [[ ! -f $BOOSTARCHIVE.tar.bz2 ]]; then
     echo "Downloading boost"
     sudo -E su kportal -c 'wget -nv http://heanet.dl.sourceforge.net/project/boost/boost/$BOOSTVERSION/$BOOSTARCHIVE.tar.bz2'
@@ -23,15 +23,15 @@ fi
 sudo chmod -R 777 "$BOOSTARCHIVE"
 cd "$BOOSTARCHIVE"
 echo "Building boost library"
-sudo ./bootstrap.sh --prefix=$HOME_DIR/usr > $INSTALL_DIR/boostinstall.log
+sudo ./bootstrap.sh --prefix=$KP_HOME/usr > $INSTALL_DIR/boostinstall.log
 echo "Installing boost library. Logs are in $INSTALL_DIR/boostinstall.log."
 sudo ./b2 install >> $INSTALL_DIR/boostinstall.log
-echo "Boost installed into $HOME_DIR/usr?"
-ls -l $HOME_DIR/usr/include/
-ls -l $HOME_DIR/usr/lib/
+echo "Boost installed into $KP_HOME/usr?"
+ls -l $KP_HOME/usr/include/
+ls -l $KP_HOME/usr/lib/
 echo "Building and installing kp_server"
-cd $ORG_DIR/src 
-export CPLUS_INCLUDE_PATH=$HOME_DIR/usr/include/
+cd $KP_HOME/src 
+export CPLUS_INCLUDE_PATH=$KP_HOME/usr/include/
 echo $CPLUS_INCLUDE_PATH
 # Test environment in sudo su
 sudo -E su kportal -c 'env | grep -i "include"'
