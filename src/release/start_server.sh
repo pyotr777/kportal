7577d4dcf0d679c9ce0279df9d7246501b70b124
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-23 11:20:23 +0900

    Detect Travis environment using $HOME. Do not restart Docker in Travis, use socat instead.

--- src/release/start_server.sh
+++ src/release/start_server.sh
@@ -3,22 +3,34 @@
 # Start Docker on port 9555 of localhost on bridge0 if,
 # create bridge0 if not exists.
 
-# Stop docker
-sudo service docker stop
+# Detect Travis
 
-# Start bridge & start docker 
-if [ ! "$(brctl show | grep bridge0)" ]; then
-    sudo brctl addbr bridge0
-    sudo ip addr add 10.0.0.1/8 dev bridge0
-    sudo ip link set dev bridge0 up
-    ip addr show bridge0
+if [[ "$HOME" = *travis* ]]; then
+	export TRAVIS=yes;
 fi
-sudo -E su kportal -c 'docker -H 127.0.0.1:9555 images &>/dev/null'
-if [[ "$?"=="1" ]]; then
-    sudo -E su kportal -c 'docker daemon -D -H 127.0.0.1:9555 -b=bridge0 > $HOME/log/docker.log &'
-fi
-# If Docker couldnt start on 9555, run socat 
-sudo -E su kportal -c 'docker -H 127.0.0.1:9555 images &>/dev/null'
-if [[ "$?"=="1" ]]; then
+
+if [[ -z $TRAVIS ]]; then
+	# Stop docker
+	sudo service docker stop
+
+	# Start bridge & start docker 
+	if [ ! "$(brctl show | grep bridge0)" ]; then
+	    sudo brctl addbr bridge0
+	    sudo ip addr add 10.0.0.1/8 dev bridge0
+	    sudo ip link set dev bridge0 up
+	    ip addr show bridge0
+	fi
+	sudo -E su kportal -c 'docker -H 127.0.0.1:9555 images &>/dev/null'
+	if [[ "$?"=="1" ]]; then
+	    sudo -E su kportal -c 'docker daemon -D -H 127.0.0.1:9555 -b=bridge0 > $HOME/log/docker.log &'
+	fi
+	# If Docker couldnt start on 9555, run socat 
+	sudo -E su kportal -c 'docker -H 127.0.0.1:9555 images &>/dev/null'
+	if [[ "$?"=="1" ]]; then
+		sudo socat TCP4-LISTEN:9555,fork UNIX-CLIENT:/var/run/docker.sock &
+	fi
+else 
 	sudo socat TCP4-LISTEN:9555,fork UNIX-CLIENT:/var/run/docker.sock &
+	export PID=$!
+	echo "Socat PID is $PID"
 fi
