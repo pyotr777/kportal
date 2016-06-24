658cb7a570d1eb41045a7965f16ccc2c84ced3be
Peter Bryzgalov <peterbryz@yahoo.com>
2016-06-24 14:50:12 +0900

    Try to start service docker, but do not stop if fails.

--- sync_down.sh
+++ sync_down.sh
@@ -5,4 +5,4 @@ else
     echo "Dry run with options \"$OPT\". To actually transfer files execute with \"do\" option: > $0 do"
 fi
 
-rsync $OPT --exclude-from 'rsyncexclude.txt' kportal:/home/kportal/Release_20160531/src/ src/
+rsync $OPT --exclude-from 'rsyncexclude.txt' kpinstall:kportal/ ./
