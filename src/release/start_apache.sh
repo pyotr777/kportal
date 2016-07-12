fbb6b0a455d8a6f72b62b6453407eaf82a331956
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-12 16:31:20 +0900

    Mount ssl/letsencrypt into Apache container. Changed kp_server running detection.

--- src/release/start_apache.sh
+++ src/release/start_apache.sh
@@ -6,7 +6,7 @@ DOCKER_HOST="-H 127.0.0.1:9555"
 
 docker $DOCKER_HOST images &>/dev/null
 if [[ $? -eq 0 ]]; then
-	docker $DOCKER_HOST run -d -p 80:80 -p 9005:443 -v /etc/kportal/www:/etc/kportal/www --name apache apachessl
+	docker $DOCKER_HOST run -d -p 80:80 -p 9005:443 -v /etc/kportal/www:/etc/kportal/www  -v /etc/kportal/ssl/letsencrypt:/etc/letsencrypt --name apache apachessl
 	if [[ $? -ne 0 ]]; then
 		echo "Couldn't start Docker container with Apache2."
 		exit 1
