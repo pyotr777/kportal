7f4c6fe63538c2ed9fb74a928dabfa127e357d2f
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-19 13:32:59 +0900

    Read KP_WEB_DNS and KP_WEB_MAIL in installkportal_ubuntu.sh. Save environment variables to $KP_HOME/ENV file. Copy this file into Apache container. Source it from install_certbot.sh and reconfigure_apache_ssl.sh

--- install_certbot.sh
+++ install_certbot.sh
@@ -6,29 +6,29 @@
 # KP_WEB_MAIL		E-mail for obtaining SSL certificates. (Used only if KP_SELF_CERT is not set).
 # This variables can also be defined in bash source file /ENV.sh
 
-export TERM="screen-256color"
-
 if [[ -f "/ENV" ]]; then
 	source /ENV
 fi
 
 if [[ ! "$KP_WEB_DNS" ]]; then
-	echo "Envvar KP_WEB_DNS is not set."
+	echo "Envvar KP_WEB_DNS is not set. Need K-portal domain name for obtaining SSL certificates."
 	echo -n "Enter domain name (without protocol name: example.com) and press [ENTER]: "
 	read KP_WEB_DNS
 fi
+if [[ ! "$KP_WEB_MAIL" ]]; then
+        echo "Need administrator's e-mail addressfor obtaining SSL certificates."
+        echo -n "Enter e-mail address and press [ENTER]: "
+        read KP_WEB_MAIL
+    fi
 
 CERT=/etc/letsencrypt/live/$KP_WEB_DNS/cert.pem
 KEY=/etc/letsencrypt/live/$KP_WEB_DNS/privkey.pem
 
+echo "Checking $CERT"
+
 if [[ ! -f "$CERT" || ! -f "$KEY" ]]; then
 	echo "Obtaining certificates from LetsEncrypt."
-	if [[ ! "$KP_WEB_MAIL" ]]; then
-		echo "Need administrator's e-mail address and your site domain name for obtaining SSL certificates."
-		echo -n "Enter e-mail address and press [ENTER]: "
-		read MAIL
-	fi
-	echo $KP_WEB_MAIL
+	echo "Using mail $KP_WEB_MAIL and DNS KP_WEB_DNS"
 
 	wget https://dl.eff.org/certbot-auto
 	chmod +x certbot-auto
