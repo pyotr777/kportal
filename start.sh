a0839806c71d7b94c90fe037813607dc931f7cba
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-21 20:07:09 +0900

    Removed webint to include in submodule in next commit.

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install python packages for webint
+# and start webint for installing K-portal.
+
+sudo apt-get update
+sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev python-lxml
+sudo pip install bottle 
+sudo pip install gevent==1.0.1
+sudo pip install bottle_websocket
+
+cd webint
+./webint.py
+
+
+
--- webint/disp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo -en "\033[38;5;70m# \033[m\n"
-echo -en "\033[38;5;70m# TEST\033[m\n"
-echo -en "\033[38;5;70m# \033[m\n"
-echo " "
