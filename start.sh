004762a424144e5738e5666a3db0abf6171cb041
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-25 17:25:44 +0900

    Run start.sh with installKP script. Need to run browser on server and ssh with -X option.

--- start.sh
+++ start.sh
@@ -2,13 +2,18 @@
 # Install python packages for webint
 # and start webint for installing K-portal.
 
+REMOTE_PORT=8080
+
 sudo apt-get update
 sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev python-lxml
 sudo pip install bottle 
 sudo pip install gevent==1.0.1
 sudo pip install bottle_websocket
 
-cd webint
+echo "Open the following address in your Internet browser to continue installation:"
+echo "http://$1:$REMOTE_PORT"
+
+cd $HOME/kportal/webint
 ./webint.py
 
 
