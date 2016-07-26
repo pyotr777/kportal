90fd9f64308396417c2fe2ab7c6e97722171ff61
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-26 10:30:44 +0900

    Run Firefox on remote server through X11. Start works fine but see that webint output is delayed compared to browser, in a while webint output freezes and firefox unexpectedly quits.

--- start.sh
+++ start.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-# Install python packages for webint
-# and start webint for installing K-portal.
+# Start Firefox on server to use as webint user interface.
 
-REMOTE_PORT=8080
+PORT=8080
 
-sudo apt-get update
-sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev python-lxml
-sudo pip install bottle 
-sudo pip install gevent==1.0.1
-sudo pip install bottle_websocket
-
-echo "Open the following address in your Internet browser to continue installation:"
-echo "http://$1:$REMOTE_PORT"
-
+echo "Opening the following address in your Internet browser to continue installation:"
+echo "http://$1:$PORT"
+echo ""
+sleep 3
 cd $HOME/kportal/webint
-./webint.py
+firefox http://localhost:$PORT
 
 
 
---
+++ start_webint_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Start webint for installing K-portal.
+
+
+cd $HOME/kportal/webint
+./webint.py
+
+
+
