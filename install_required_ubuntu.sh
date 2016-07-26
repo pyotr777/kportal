2954314405cfb25f64572225886b2034779e02ec
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-26 10:30:44 +0900

    Run Firefox on remote server through X11. Start works fine but see that webint output is delayed compared to browser, in a while webint output freezes and firefox unexpectedly quits.

---
+++ install_required_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install python packages for webint
+
+sudo apt-get update
+sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev python-lxml firefox
+sudo pip install bottle 
+sudo pip install gevent==1.0.1
+sudo pip install bottle_websocket
