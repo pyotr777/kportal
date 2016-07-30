ecfc0d946082c84054c1206eeb30a87542261e12
Peter Bryzgalov <peterbryz@yahoo.com>
2016-07-30 14:06:15 +0900

    Synchronisation scripts require remote hostname and path.

--- sync_down.sh
+++ sync_down.sh
@@ -1,8 +1,18 @@
 OPT="-anv"
-if [[ $1 == "do" ]]; then
+server="kpinstall"
+if [[ $1 != "" ]]; then
+    if [[ $1 == "-do" ]]; then
+        OPT="-av"
+    else
+        echo "Connecting to $1"
+        server="$1"
+    fi
+fi
+if [[ $2 == "-do" ]]; then
     OPT="-av"
-else
-    echo "Dry run with options \"$OPT\". To actually transfer files execute with \"do\" option: > $0 do"
+fi
+if [[ "$OPT" == *n* ]]; then
+    echo "Dry run with options \"$OPT\". To actually transfer files execute with \"-do\" option: > $0 [server] -do"
 fi
 
-rsync $OPT --exclude-from 'rsyncexclude.txt' kpinstall:kportal/ ./
+rsync $OPT --exclude-from 'rsyncexclude.txt' $server ./
