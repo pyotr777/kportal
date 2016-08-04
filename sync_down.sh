e94dcf5aeef840eda422e7f0bd7b6a044ab9771c
Peter Bryzgalov <peterbryz@yahoo.com>
2016-08-04 12:25:45 +0900

    Modified install script for use on remote machine with distribution files. Synchronisation scripts modified to allow use of more options (ssh keys etc.).

--- sync_down.sh
+++ sync_down.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 # Syncronise files with ~/kportal directory on the host.
 
+read -rd '' USAGE <<EOF
+	Script for copying updated K-portal installation files to remote machine.
+	Usage:
+	$(basename $0) [-i ssh key] [-e exclude pattern] [user@]host [-do]
+	Whithout "-do" option a dry run will be perfomed. Use it to see the list of updated files.
+EOF
+
 OPT="-anv"
 DIR="kportal"
+KEY=""
+EXCLUDE=""
 
-if [[ $1 != "" ]]; then
-    if [[ $1 == "-do" ]]; then
-        OPT="-av"
-    else
-        echo "Connecting to $1"
-        server="$1"
-    fi
-fi
 
-if [[ $2 == "-do" ]]; then
-    OPT="-av"
-fi
+while test $# -gt 0; do
+	case "$1" in
+		-i)
+			KEY="-e 'ssh -i $2'";shift;
+			;;
+		-e)
+			EXCLUDE="$EXCLUDE --exclude '$2'";shift;
+			;;
+		-do)
+			OPT="-av"
+			echo "Live run"
+			;;		
+		--) 
+			shift
+			break;;
+		-*)	
+			echo "Invalid option: $1"
+			echo "$USAGE"
+			exit 1;;
+		*)
+			server="$1"
+			;;
+	esac
+	shift
+done
+
+
 if [[ "$OPT" == *n* ]]; then
-    echo "Dry run with options \"$OPT\". To actually transfer files execute with \"-do\" option: > $0 [server] -do"
+	echo "Dry run with options \"$OPT\". To actually transfer files run with \"-do\" option."
 fi
 
-rsync $OPT --exclude-from 'rsyncexclude.txt' $server:$DIR/ ./
+eval rsync $OPT $KEY $EXCLUDE --exclude-from 'rsyncexclude.txt' $server:$DIR/ ./
