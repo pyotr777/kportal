61c0526ee9ca99b7086708bb68919191cdee6345
Peter Bryzgalov <peterbryz@yahoo.com>
2016-09-29 12:30:59 +0900

    Updates to staging test app.

--- sample_application/staging_test/job_template.sh
+++ sample_application/staging_test/job_template.sh
@@ -5,18 +5,17 @@
 #PJM --stg-transfiles all
 #PJM --stgin-dir "./staging_test ./"
 #PJM --stgout "./* ./"
-#PJM --stgout-dir "./stgoutdir ./"
+#PJM --stgout-dir "./stgoutdir ./stgoutdir"
 . /work/system/Env_base
 echo "On node $(hostname -f)"
 pwd
 ls -l
 echo "Stage out dir is stgoutdir."
 mkdir stgoutdir
-touch stgoutdir/stgout_test.txt
-filename="COMPNODETEST_$(hostname -d).txt"
+filename="stgoutdir/compnodetest_$(hostname -f).txt"
 echo "Creating file $filename"
 touch $filename
 echo "This file created on node: $(hostname -f)" >> $filename
 echo "$(hostname -a)" >> $filename
-ls -l
+find .
 
