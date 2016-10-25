18f19b85021f9b9e2cca75d214af6507d2c1f0bd
Peter Bryzgalov <peterbryz@yahoo.com>
2016-10-25 16:24:03 +0900

    Updated image and movie makers for mixerVessel2D sample.

--- visual_server/make_images.py
+++ visual_server/make_images.py
@@ -1,3 +1,6 @@
+# Produce image series from simulation data in VTK format
+
+
 import numpy as np
 from vtk.util.numpy_support import vtk_to_numpy
 import matplotlib as mpl
@@ -8,25 +11,34 @@ import vtk
 from mpl_toolkits.mplot3d import Axes3D
 import os
 
-
-def getData_vtk(filename):
+def getData(filename):
     # load a vtk file as input
     reader = vtk.vtkUnstructuredGridReader()
     reader.SetFileName(filename)
     reader.Update()
-    print reader.GetOutput().GetPoints()
-    # Get the coordinates of nodes in the mesh
-    nodes_vtk_array= reader.GetOutput().GetPoints().GetData()
-    pressure_vtk_array = reader.GetOutput().GetPointData().GetArray(0)
-    speed_vtk_array = reader.GetOutput().GetPointData().GetArray(1)
-    nodes = vtk_to_numpy(nodes_vtk_array)
-    p = vtk_to_numpy(pressure_vtk_array)
-    U  = vtk_to_numpy(speed_vtk_array)
-    return nodes, p, U
+    vtk_arrays= reader.GetOutput().GetPointData()
+    #print vtk_nodes
+    #print vtk_arrays
+    return vtk_arrays
 
-def extractFlatData(nodes, p, U):
+def getNodes(filename):
+    # load a vtk file as input
+    reader = vtk.vtkUnstructuredGridReader()
+    reader.SetFileName(filename)
+    reader.Update()
+    points = reader.GetOutput().GetPoints()
+    if points is None:
+        return None
+#    print points
+#    Get the coordinates of nodes in the mesh
+    nodes_vtk_array= reader.GetOutput().GetPoints().GetData()
+    nodes = vtk_to_numpy(nodes_vtk_array)
+    return nodes
+
+# Remove data for z != 0
+def extractFlatData(nodes, a, U):
     merged = np.empty([len(nodes),6])
-    merged[:,0] = p
+    merged[:,0] = a
     merged[:,1:3] = U[:,:2]
     merged[:,3:] = nodes
     # Merged z0 and z1 arrays should be same because of
@@ -34,82 +46,125 @@ def extractFlatData(nodes, p, U):
     merged_z0 = merged[np.where(merged[:,5] == 0)]
     merged_z1 = merged[np.where(merged[:,5] != 0)]
     # Extract nodes, p and U arrays from z0 array
-    p = merged_z0[:,0]
+    a = merged_z0[:,0]
     U = merged_z0[:,1:3]
     nodes = merged_z0[:,3:5]
-    return nodes, p, U
+    return nodes, a, U
 
-def plot2D(nodes, a, vmin, vmax):
-    # 2d plotting
-    fig = plt.figure(figsize=(8,6))
-    color_map = plt.cm.get_cmap('viridis')
-    axes = plt.gca()
-    #axes.set_xlim([-.1,2.1])
-    #axes.set_ylim([-.01,0.11])
-    #plt.axis('off')
+# Remove data for |U| == 0 and z != 0
+def extractNonzeroData(nodes, a, U):
+    merged = np.empty([len(nodes),7])
+    print "Extrcting |U|!=0.\na:{} U:{} nodes:{}".format(a.shape, U.shape, nodes.shape)
+    merged[:,0] = a
+    merged[:,1:3] = U[:,:2]
+    merged[:,3:6] = nodes
+    # U length squared ^2
+    merged[:, 6] = np.power(U[:,0],2) + np.power(U[:,1],2)
+    merged_nonzero = merged[np.where(merged[:,6] > 0.01)]
+    merged_nonzero = merged_nonzero[np.where(merged_nonzero[:,5] == 0)]
+    print "Nonzero matrix shape: {}".format(merged_nonzero.shape)
+    # Extract nodes, p and U arrays from z0 array
+    a = merged_nonzero[:,0]
+    U = merged_nonzero[:,1:3]
+    nodes = merged_nonzero[:,3:5]
+    return nodes, a, U
+
+# Plot vector field on existing figure
+def plotVector_combined(nodes, U, vmin, vmax, filename=""):
+    X = nodes[:,0]
+    Y = nodes[:,1]
+    UN = U[:,0]
+    VN = U[:,1]
+    stride_elmts = 5
+    Xsub = X[::stride_elmts]
+    Ysub = Y[::stride_elmts]
+    UNsub= UN[::stride_elmts]
+    VNsub= VN[::stride_elmts]
+
+    plt.quiver(Xsub, Ysub, UNsub, VNsub,        # data
+               np.hypot(UNsub, VNsub),
+ #              Usub,                   # colour the arrows based on this array
+ #              color='#ffee77',
+               cmap=plt.cm.YlOrRd,
+               norm=mpl.colors.Normalize(vmin=vmin,vmax=vmax), # Colour range fix
+               linewidth=0.5,
+               scale=10
+               )
+
+    plt.colorbar()                  # adds the colour bar
+
+# Plot 2D data on existing figure
+def plot2D_combined(nodes, a, vmin, vmax):
+    cmap = mpl.cm.seismic
+    color_map = plt.cm.get_cmap('GnBu')
     sc = plt.scatter(nodes[:,0],nodes[:,1],
-                     s=320,
-#                     color=cmap(a / a_max),
+                     s=550,
                      c = a,
                      cmap = color_map,
                      vmin = vmin,
                      vmax = vmax,
                      linewidth=0,
-                     marker="s")
-    plt.colorbar(sc)
-    fig.tight_layout()
+                     marker="o")
 
-def plotVector(X, Y, UN, VN, U, vmin, vmax, filename=""):
-    plot1 = plt.figure(figsize=(14,10))
-    plt.quiver(X, Y, UN, VN,        # data
-               U,                   # colour the arrows based on this array
-               cmap=plt.cm.plasma,     # colour map
-               norm=mpl.colors.Normalize(vmin=vmin,vmax=vmax), # Colour range fix
-               headlength=4,
-               width=0.007,
-               scale_units='xy',
-               scale=9,
-               linewidths=1,
-               edgecolors='k'
-               )
 
-    plt.colorbar()                  # adds the colour bar
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Velocity vector')
-    if (filename != ""):
-        plot1.tight_layout()
-        plt.savefig(filename,bbox_inches='tight')
-    else:
-        plt.show(plot1)                 # display the plot
-    plt.close(plot1)
+# Generate source data file name and
+# image file name for exporting visualisation frame.
+def generateFilename(base, vtkdir, imdir, i, n):
+    source = os.path.join(vtkdir, base + str(i) + ".vtk")
+    img = os.path.join(imdir,"img_"+str(n).zfill(4)+".png")
+    return source, img
+
 
 vtkdir = "VTK"
-vmin = 0
-vmax = 1
 imdir = "export"
+filename_base = "mixerVessel2D_"
+max_i = 500
 
+# Main function that reads source files and produces images
 def make_images():
     try:
         os.stat(imdir)
     except:
         os.mkdir(imdir)
 
-    n = 0
-    for i in range(0,501,50):
-        print i
-        filename = vtkdir+"/cavity_"+str(i) + ".vtk"
-        im_filename = imdir + "/" + "cavity_" + str(n).zfill(4) + ".png"
-        print "From {} to {}".format(filename, im_filename)
-        n += 1
+    n = 0   # image counter
+    for i in range(0,max_i+1):
+        src_file, img_file = generateFilename(filename_base, vtkdir, imdir, i, n)
+        # print "Looking for " + src_file
+        if os.path.isfile(src_file):
+            print "Found " + src_file
+            n += 1
+            filename = src_file
+            vtk_data = getData(filename)
+            nodes = getNodes(filename)
+            alpha = vtk_to_numpy(vtk_data.GetArray(1))
+            U = vtk_to_numpy(vtk_data.GetArray(3))
+            a = alpha
 
-        nodes, p , U   = getData_vtk(filename)
-        nodes_half, p_half, U_half = extractFlatData(nodes, p, U)
-        Un_half = np.sqrt(U_half[:,0]**2 + U_half[:,1]**2) #Randomisation:  + np.random.rand(U_half.shape[0])
-        print(np.amin(Un_half),np.amax(Un_half))
+            vmin = np.nanmin(a)
+            vmax = np.nanmax(a)
+            print "Alpha range: {} - {}, shape: {}".format(vmin,vmax, a.shape)
 
-        plotVector(nodes_half[:,0], nodes_half[:,1], U_half[:,0], U_half[:,1],
-                   Un_half, vmin, vmax, filename = im_filename)
+            nodes_half, a_half, U_half = extractFlatData(nodes, a, U)
+            nodes_nonzero, a_nonzero, U_nonzero = extractNonzeroData(nodes, a, U)
+
+            # Plot combined figure
+            plt.figure(figsize=(14,12))
+            axes = plt.gca()
+            plt.axis([-.1, .1, -.1, .1])
+            plt.xlabel('X')
+            plt.ylabel('Y')
+            plt.axis('off')
+
+            plt.title('Alpha water and Velocity vector')
+            plot2D_combined(nodes_half, a_half, vmin, vmax)
+            plotVector_combined(nodes_nonzero, U_nonzero, 0, 0.4)
+
+            plt.savefig(img_file,bbox_inches='tight')
+            plt.close()
+
 
 if __name__ == "__main__":
     make_images()
+
+
--- visual_server/make_movie.sh
+++ visual_server/make_movie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Making movie out.mp4"
-ffmpeg -framerate 5 -i export/cavity_%04d.png -vcodec mpeg4 -r 5 out.mp4
+ffmpeg -framerate 10 -i export/img_%04d.png -vcodec mpeg4 -q:v 2 -r 10 out.mp4
