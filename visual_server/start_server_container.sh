36ccad3cd7c34f96837fb3550dd49fb33f6cb63c
Peter Bryzgalov <peterbryz@yahoo.com>
2016-11-28 18:40:04 +0900

    Updated visual server to start flask on container start.

--- visual_server/start_server_container.sh
+++ visual_server/start_server_container.sh
@@ -2,4 +2,4 @@
 
 # Start container with Flask visualisation server, mounting current dirctory.
 
-docker run -ti --rm --name flask -v $(pwd):/root/server -p 8080:5000 pyotr777/flask bash
+docker run -ti --rm --name flask -v $(pwd):/root/server -p 8080:5000 pyotr777/vis-serv
